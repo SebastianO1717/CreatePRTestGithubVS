5659260fa3d8d8bac0e21a0f50756a1038a6e8b3
Sebastian Hernandez <sebaoctavio@gmail.com>
2023-07-05 14:25:27 -0700

    sync operation

--- CreatePRTestGithub/Program.cs
+++ CreatePRTestGithub/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!"); // syncing changes in main
 // create PR from main to main
